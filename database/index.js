2520dffb8dde9458f758088fcd5b905c81718345
Zauro25 <221506134@webmail.uad.ac.id>
2024-07-26 16:30:22 +0700

    uhuyy

--- database/index.js
+++ database/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mysql = require('mysql2');
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
@@ -16,22 +15,4 @@ pool.getConnection((err, conn) => {
 });
 
 module.exports = pool.promise();
-=======
-const mysql = require('mysql');
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
-});
 
-pool.getConnection((err, conn) => {
-  if (err) console.log(err);
-  else console.log("Connected successfully");
-});
-
-module.exports = pool.promise();
->>>>>>> e745e19266cbcd4fce95b54b768a1d4b9b555410
